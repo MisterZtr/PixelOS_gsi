dd54b3b139e005831fd520fc66bc5305752bf56a
Andrew <87061244+MisterZtr@users.noreply.github.com>
2023-11-14 19:06:17 +0300

    Cleanup makefile

--- pixel.mk
+++ pixel.mk
@@ -1,26 +1,10 @@
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_tablet.mk)
 $(call inherit-product, vendor/aosp/config/BoardConfigSoong.mk)
 $(call inherit-product, device/lineage/sepolicy/common/sepolicy.mk)
 -include vendor/aosp/build/core/config.mk
 TARGET_NO_KERNEL_OVERRIDE := true
 TARGET_NO_KERNEL_IMAGE := true
-TARGET_BOOT_ANIMATION_RES := 1080
-
-TARGET_USES_PREBUILT_VENDOR_SEPOLICY := true
-TARGET_HAS_FUSEBLK_SEPOLICY_ON_VENDOR := true
+TARGET_BOOT_ANIMATION_RES := 720
 
 TARGET_FACE_UNLOCK_SUPPORTED := true
-
-# Use CCache
-USE_CCACHE := true
-
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-	persist.sys.binary_xml=false \
-	dalvik.vm.dex2oat64.enabled=true \
-
-#prebuilts
-PRODUCT_PACKAGES += \
-  OpenEUICC \
-
-# APN
-PRODUCT_PACKAGES += apns-conf.xml
